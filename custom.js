289def83c3c36312e1ad793cbeca088a733aca64
waelmb <wafm2007@gmail.com>
2021-03-16 13:43:57 -0500

    added learn page and timer during story

--- custom.js
+++ custom.js
@@ -1,3 +1,4 @@
+/*
 var choices = document.getElementsByClassName('choice');
 
 for(var i = 0; i < choices.length; i++) {
@@ -13,4 +14,24 @@ for(var i = 0; i < choices.length; i++) {
             elements[i].style.animation = ''
         }
   })
+}*/
+
+var seconds = 13;
+
+function countDown() {
+    seconds -= 1;
+    console.log(seconds)
+
+    if(seconds < 3) {
+        window.location.replace("outcome11.html");
+    }
+    else if(seconds < 10) {
+        var elements = document.getElementsByClassName('container');
+        for(var i = 0; i < elements.length; i++) {
+            elements[i].style.animation = ''
+            elements[i].style.animation = 'blinkAnimation' + seconds + ' ' + seconds/10 + 's infinite'
+        }
+    }
 }
+
+setInterval(countDown, 1000);
---
+++ forward.js
@@ -0,0 +1,4 @@
+document.getElementById("buildWeapons").addEventListener("click", function() {
+    console.log('buildWeapons was clicked')
+    window.location.replace("story2.html");
+  });
